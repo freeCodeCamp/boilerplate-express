4908a01f21f9067de15edb62fc2af592c1c27a83
Richard Borges <richardborgescc@users.noreply.github.com>
2019-04-18 22:15:59 -0300

    APIs and microservices. Node js: send html file to the client.

--- myApp.js
+++ myApp.js
@@ -12,11 +12,16 @@ var app = express();
 console.log("Hello World");
 
 /** 2) A first working Express Server */
+/*
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
+*/
 
 /** 3) Serve an HTML file */
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
+});
 
 
 /** 4) Serve static assets  */
