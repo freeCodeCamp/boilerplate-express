364e74f89e5d45b0ee4c14af202117ad970d5efd
Glitch (fcc-startsic-node-express-c) <none>
2020-11-03 14:00:15 +0000

    📹😘 Updated with Glitch

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# [Introduction to the Basic Node and Express Challenges](https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/)
+A simple node express app running on Glitch
+=================
+
+* Click `Show` in the header to see your app live. Updates to your code will instantly deploy and update live.
+* If this is not YOUR project, you can remix it by...
+  * clicking the title in the top left, and choosing Remix project, then
+  * renaming the project so you know it's yours
+
+Your Project
+------------
+
+- your express app starts at `server.js`
+- add frameworks and packages in `package.json`
+- read about [Basic routing](https://expressjs.com/en/starter/basic-routing.html)
+
+Advanced note: This version includes CORS middleware so that your JSON can be consumed by a fetch from javascript loaded from some other site.
--- myApp.js
+++ myApp.js
@@ -1,29 +1,29 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
 
 // --> 7)  Mount the Logger middleware here
-
+// app.use("/", (req, res, next) => {
+  // const stringM = req.method + " " + req.path + " - " + req.ip;
+  // console.log(stringM);
+  // res.send(stringM);
+  // next();
+// });
 
 // --> 11)  Mount the body-parser middleware  here
-// app.use(bodyParser.json());
 
 /** 1) Meet the node console. */
 // app.get("/", (req,res) => {
-  // console.log("Hello World");
+// console.log("Hello World");
 // });
 
 /** 2) A first working Express Server */
 // app.get('/', (req,res)=>{
-  // res.send('Hello Express');
+// res.send('Hello Express');
 // });
 
 /** 3) Serve an HTML file */
 // app.get('/', (req,res)=>{
-  // res.sendFile(__dirname + '/views/index.html')
+// res.sendFile(__dirname + '/views/index.html')
 // });
 
 /** 4) Serve static assets  */
@@ -31,34 +31,47 @@ var bodyParser = require('body-parser');
 
 /** 5) serve JSON on a specific route */
 // app.get('/json', (req, res) => {
-  // res.json({"message": "Hello json"});
+// res.json({"message": "Hello json"});
 // });
 
 /** 6) Use the .env file to configure the app */
- app.get('/json', (req, res)=>{
-   if(process.env.MESSAGE_STYLE==='uppercase' ){
-     res.json({"message": "HELLO JSON"});
-   }else{
-      res.json({"message": "Hello json"});
-   }
- });
- 
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
+
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
-
 /** 8) Chaining middleware. A Time server */
-
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({ time: req.time });
+  }
+);
 
 /** 9)  Get input from client - Route parameters */
-
+app.get('/:word/echo', (req, res)=> {
+  const word = req.params.word;
+  res.json({echo: word});
+});
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 
-  
+
+
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
+
 
 
 /** 12) Get data form POST  */
@@ -67,44 +80,8 @@ var bodyParser = require('body-parser');
 
 // This would be part of the basic setup of an Express app
 // but to allow FCC to run tests, the server is already active
-app.listen(process.env.PORT || 3000 );
+/** app.listen(process.env.PORT || 3000 ); */
 
-/* //---------- DO NOT EDIT BELOW THIS LINE --------------------
+//---------- DO NOT EDIT BELOW THIS LINE --------------------
 
- module.exports = app; */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
