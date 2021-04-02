31699f5bb622e940475760caec90ae5b97efda7b
Nana aka y_chris <78367440+nana-sv@users.noreply.github.com>
2021-04-02 21:23:59 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,53 @@
 var express = require('express');
 var app = express();
 
+//next
+
+/** 4) Serve static assets  */
+app.use(express.static(__dirname + '/public/'));
+
+app.get("/", function(req, res) {
+res.sendFile("/views/index.html", { root : __dirname});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
