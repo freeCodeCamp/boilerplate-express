4e94ff8758a817bda90e48c0abb61534bae2339a
Indu rupi <indu795@gmail.com>
2021-05-08 09:38:23 +0530

    home page to send index html instead of string

--- myApp.js
+++ myApp.js
@@ -1,35 +1,9 @@
 var express = require('express');
 var app = express();
 
+const absolutePath = __dirname + "/views/index.html"
 app.get("/", function(req, res) {
-  res.send("Hello Express !")
+  res.sendFile(absolutePath);
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
  module.exports = app;
