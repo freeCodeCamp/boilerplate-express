60939d806e0f8d8190ab913aef1ea381a7cbd46d
Indu rupi <indu795@gmail.com>
2021-05-10 12:21:09 +0530

    static directory

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var app = express();
 
 const absolutePath = __dirname + "/views/index.html"
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
   res.sendFile(absolutePath);
