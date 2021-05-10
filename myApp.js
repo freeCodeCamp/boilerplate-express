b7a778d56ff347629be1c41b3a3e0f94fc5e6567
Indu rupi <indu795@gmail.com>
2021-05-10 12:22:28 +0530

    absolute path for static files added

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var app = express();
 
 const absolutePath = __dirname + "/views/index.html"
 
-app.use(express.static("public"));
+app.use(express.static(__dirname));
 
 app.get("/", function(req, res) {
   res.sendFile(absolutePath);
