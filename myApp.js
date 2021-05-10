9651978ce497aac7bb37842114c5dc58d3412d5a
Indu rupi <indu795@gmail.com>
2021-05-10 12:15:05 +0530

    added assets

--- myApp.js
+++ myApp.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 
 const absolutePath = __dirname + "/views/index.html"
+
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", function(req, res) {
   res.sendFile(absolutePath);
 })
 
- module.exports = app;
+module.exports = app;
