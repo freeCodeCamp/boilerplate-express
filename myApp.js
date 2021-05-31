28c631b526b6c5973d57f58e0cf2f509fad402c4
root <root@DESKTOP-KALQFR4.localdomain>
2021-05-31 13:42:50 -0400

    added json

--- myApp.js
+++ myApp.js
@@ -9,4 +9,10 @@ app.get('/',(req,res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/json',(req,res) => {
+    res.json({
+        "message": "Hello json"
+    });
+});
+
  module.exports = app;
