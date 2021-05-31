5c48fa4658d47ea9ee00d9810f1ff825c33d8627
root <root@DESKTOP-KALQFR4.localdomain>
2021-05-31 13:36:23 -0400

    Use middleware

--- myApp.js
+++ myApp.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 
+
+app.use('/public',express.static(__dirname +'/public/'));
+
 app.get('/',(req,res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
