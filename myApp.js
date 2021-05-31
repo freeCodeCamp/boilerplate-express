3b8b7dc91d51d8541946bafafcd9c1dcafde4b2f
root <root@DESKTOP-KALQFR4.localdomain>
2021-05-31 13:08:10 -0400

    serve html file

--- myApp.js
+++ myApp.js
@@ -1,43 +1,9 @@
 var express = require('express');
 var app = express();
-console.log("Hello World")
+console.log("Hello World");
+
 app.get('/',(req,res) => {
-    res.send('Hello Express');
+    res.sendFile(__dirname + '/views/index.html');
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
  module.exports = app;
