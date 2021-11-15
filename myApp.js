448c6d7528e3a7e6d7f1caf30c63612b6bdc0ba1
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-11-15 01:20:27 +0000

    feat: serve static assets

--- myApp.js
+++ myApp.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.use("/public", express.static(__dirname + "/public"))
+    
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-console.log("Hello World")
+
 
 
 
