091e91149d10334cda5ec4fc31e4af84caecddec
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-11-15 13:16:54 +0000

    feat: use the env file

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
+require("dotenv").config();
 
-app.use("/public", express.static(__dirname + "/public"))
+app.use("/public", express.static(__dirname + "/public"));
     
 
 app.get("/", function (request, response) {
@@ -9,12 +10,16 @@ app.get("/", function (request, response) {
 });
 
 app.get("/json", (request, response) => {
-  response.json({ message: 
-                    "Hello json"
-                });
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+     response.send({ message: "Hello json".toUpperCase()});
+    } else {
+    response.send({ message: "Hello json"});
+    }
+    
 });
 
 
 
 
- module.exports = app;
+
+module.exports = app;
