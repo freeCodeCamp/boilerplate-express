f3a2492fea67c69210cdb2bfa0cfe8a0ec4a5519
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-11-15 01:13:09 +0000

    feat: serve an HTML file

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get("/", function (request, response) {
-  response.send("Hello Express");
+  response.sendFile(__dirname + "/views/index.html");
 });
 
 console.log("Hello World")
