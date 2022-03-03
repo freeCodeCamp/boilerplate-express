b281921d6724963ec69075dcd486619206650f44
E Brian Phillips <ebrianphillips@outlook.com>
2022-03-03 15:24:12 -0600

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -1,5 +1,36 @@
+//let mySecret = process.env['MESSAGE_STYLE']
 var express = require('express');
 var app = express();
+var myMessage = 'Hello json';
+console.log("Hello World");
+
+
+
+
+app.get("/", (req, res) => {
+    res.sendFile(
+      __dirname +  "/views/index.html"
+      );
+     
+});
+
+
+app.get("/json", (req, res) => {
+
+    if (process.env['MESSAGE_STYLE'] === "uppercase") {
+         res.json({ "message": "HELLO JSON" })
+    } else {
+         res.json({ "message": "Hello json" })
+    }
+    
+    
+
+  })
+//
+  
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
@@ -28,12 +59,5 @@ var app = express();
 
 
 
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
+clon
