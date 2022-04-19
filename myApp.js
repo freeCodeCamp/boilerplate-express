d04a9327e3c53722681a781a7f6189ec2a705694
elkarroumytaha <elkarroumytaha@gmail.com>
2022-04-19 01:35:06 +0000

    make it better

--- myApp.js
+++ myApp.js
@@ -3,7 +3,9 @@ var app = express();
 console.log("Hello World");
 
 app.get('/',function(req,res){
-  res.send("Hello Express");
+  //res.send("Hello Express");
+  let indexPage = __dirname + "/views/index.html";
+  res.sendFile(indexPage);
 });
 
 
