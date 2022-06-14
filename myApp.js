a40f3d5215fc42659637f681088c499a15dba378
Sr-Kamogelo <kamogelooliphant140@gmail.com>
2022-06-14 13:44:14 +0200

    change

--- myApp.js
+++ myApp.js
@@ -18,7 +18,7 @@ app.get("/", function(req,res){
 });
 
 // 4. getting a staic file of css 
-app.get("/public", express.static(__dirname + "/public/style.css"));
+app.get(express.static(__dirname + "/public/style.css"));
 
 
 
