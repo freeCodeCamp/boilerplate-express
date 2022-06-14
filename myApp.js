2806f94721fa1186eafd91de88d665bcac1b3fbf
Sr-Kamogelo <kamogelooliphant140@gmail.com>
2022-06-14 12:32:49 +0200

    change

--- myApp.js
+++ myApp.js
@@ -4,12 +4,17 @@ let app = express();
 //onsole.log("Hello World");
 
 /*-----------------------------------------------------------
- The get is the express app method.
+ 2. The get is the express app method.
 The "/" is the root path which the network will take to get to your website path.
 There is the handler which is the function that will handle the request and the responss
 ------------------------------------------------------------*/
-app.get("/", function(req, res){
+/*app.get("/", function(req, res){
     res.send("Hello Express");
+});*/
+
+// 3. sendFile method goes and gets an static file to present to the page
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 
