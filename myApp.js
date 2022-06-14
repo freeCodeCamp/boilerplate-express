6bc628747fc040b8d157d760aabdb8422c64348d
Sr-Kamogelo <kamogelooliphant140@gmail.com>
2022-06-14 11:08:25 +0200

    change

--- myApp.js
+++ myApp.js
@@ -1,9 +1,16 @@
 let express = require('express');// Light weight js  web framework
 let app = express();
 
-console.log("Hello World");
+//onsole.log("Hello World");
 
-
+/*-----------------------------------------------------------
+ The get is the express app method.
+The "/" is the root path which the network will take to get to your website path.
+There is the handler which is the function that will handle the request and the responss
+------------------------------------------------------------*/
+app.get("/", function(req, res){
+    res.send("Hello World");
+});
 
 
 
