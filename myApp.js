b3ff4a1e097ac8a9d14ff00c6e12d5981eeb7e99
kevoru18 <kevoru@gmail.com>
2022-06-15 11:47:56 -0600

    Se corrigio path

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-absolutePath=__dirname + views/index.html
+absolutePath=__dirname + "/views/index.html";
 console.log("Hello World");
 app.get("/",(req,res)=>{
     res.sendFile(absolutePath)
