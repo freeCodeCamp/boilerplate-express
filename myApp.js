1283d87c1222af05d8c0148d811e693d76aa6810
Sr-Kamogelo <kamogelooliphant140@gmail.com>
2022-06-15 12:10:31 +0200

    change

--- myApp.js
+++ myApp.js
@@ -43,8 +43,14 @@ app.get("/json",(req,res)=>{
          }
 });
 
-
-
+// 7. Chain middleware for time server
+app.get("/now", (req,res,next)=>{
+    req.time = new Date().toString();
+    next();
+},
+(req,res)=>{
+    res.send({time:req.time});
+});
 
 
 
