f20b49fac5297c7ab477be0950d51ff080bd9ded
kevoru18 <kevoru@gmail.com>
2022-06-16 12:27:45 -0600

    mas cambios

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+
+
 require("dotenv").config()
 let express = require('express');
 let app = express();
@@ -8,11 +10,10 @@ app.get("/",(req,res)=>{
     res.sendFile(absolutePath)
 })
 app.use("/public",express.static(ruta))
-var mensaje=process.env['MESSAGE_STYLE']
 
-app.get("/json",(req,res)=>{
-    
-     if(mensaje==='uppercase'){
+app.get('/json',(req,res)=>{
+    const mensaje=process.env['MESSAGE_STYLE']
+     if(mensaje==="uppercase"){
         res.json(
             {
                 "message":"Hello json".toUpperCase()
