35c188f13b23df4ea36b62c19b09ea5f0038eeef
kevoru18 <kevoru@gmail.com>
2022-06-16 13:32:34 -0600

    mas

--- myApp.js
+++ myApp.js
@@ -3,25 +3,25 @@
 require("dotenv").config()
 let express = require('express');
 let app = express();
-absolutePath=__dirname + "/views/index.html";
-ruta=__dirname+"/public"
+// absolutePath=__dirname + "/views/index.html";
+// ruta=__dirname+"/public"
 console.log("Hello World");
 // app.get("/",(req,res)=>{
 //     res.sendFile(absolutePath)
 // })
 app.use("/public",express.static(ruta))
-const mensaje=process.env['MESSAGE_STYLE']
-app.get('/json',(req,res)=>{
+app.get('/json',(function(req,res){
    
+const mensaje=process.env['MESSAGE_STYLE'];
      if(mensaje==="uppercase"){
         res.json(
-            {"message": "HELLO JSON"}
+            {"message": "Hello json".toUpperCase()}
             );
     
      } else{
         res.json({"message": "Hello json"})
      }
-    });
+    }));
 
 
 
