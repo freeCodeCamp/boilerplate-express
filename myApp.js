87fa2b9dc517300306e8f1c2790820b77d7789dc
kevoru18 <kevoru@gmail.com>
2022-06-16 13:18:34 -0600

    cambios

--- myApp.js
+++ myApp.js
@@ -6,9 +6,9 @@ let app = express();
 absolutePath=__dirname + "/views/index.html";
 ruta=__dirname+"/public"
 console.log("Hello World");
-app.get("/",(req,res)=>{
-    res.sendFile(absolutePath)
-})
+// app.get("/",(req,res)=>{
+//     res.sendFile(absolutePath)
+// })
 app.use("/public",express.static(ruta))
 const mensaje=process.env['MESSAGE_STYLE']
 app.get('/json',(req,res)=>{
