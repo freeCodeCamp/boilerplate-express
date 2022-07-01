a06cc01b107fde93b5fb995954a5f49d04d97bc5
Lucas Rodriguez <83020964+lrodriguez911@users.noreply.github.com>
2022-07-01 15:29:50 +0000

    add method sendFile

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-app.get("/", (req, res)=> res.send("Hello Express"))
+/* app.get("/", (req, res)=> res.send("Hello Express")) */
+let htmlPath = __dirname + "/views/index.html" ;
+app.get("/", (req, res)=> res.sendFile(htmlPath))
 
 
 
