36da7302e06a96a714db325b7562b39bac98ef5f
Lucas Rodriguez <83020964+lrodriguez911@users.noreply.github.com>
2022-07-03 00:12:38 +0000

    add express static

--- myApp.js
+++ myApp.js
@@ -3,7 +3,8 @@ let app = express();
 console.log("Hello World");
 /* app.get("/", (req, res)=> res.send("Hello Express")) */
 let htmlPath = __dirname + "/views/index.html" ;
-app.get("/", (req, res)=> res.sendFile(htmlPath))
+app.get("/", (req, res)=> res.sendFile(htmlPath));
+express.static("/public")
 
 
 
