7348d62087fa0343b65bd7a69f6a4a526b5dfaaf
Lucas Rodriguez <83020964+lrodriguez911@users.noreply.github.com>
2022-07-03 23:34:45 +0000

    add method use whit express static

--- myApp.js
+++ myApp.js
@@ -3,9 +3,11 @@ let app = express();
 console.log("Hello World");
 /* app.get("/", (req, res)=> res.send("Hello Express")) */
 let htmlPath = __dirname + "/views/index.html" ;
+let htmlPublic = __dirname + "/public/style.css";
 app.get("/", (req, res)=> res.sendFile(htmlPath));
-express.static("/public")
+app.use("/", express.static(htmlPublic));
 //add method express static
+//the method app.get use to send the petition get to server declaraded up
 
 
 
