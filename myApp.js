1e0fb4468df36bd3f6038c10f5c0f5ddcf16d37e
Lucas Rodriguez <83020964+lrodriguez911@users.noreply.github.com>
2022-07-04 00:06:56 +0000

    add app use usefull

--- myApp.js
+++ myApp.js
@@ -2,10 +2,10 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 /* app.get("/", (req, res)=> res.send("Hello Express")) */
-let htmlPath = __dirname + "/views/index.html" ;
-let htmlPublic = __dirname + "/public/style.css";
-app.get("/", (req, res)=> res.sendFile(htmlPath));
-app.use("/", express.static(htmlPublic));
+let htmlPath = __dirname + "/views/index.html";
+let htmlPublic = __dirname + "/public";
+app.get("/", (req, res) => res.sendFile(htmlPath));
+app.use("/public",express.static(htmlPublic));
 //add method express static
 //the method app.get use to send the petition get to server declaraded up
 
@@ -46,4 +46,4 @@ app.use("/", express.static(htmlPublic));
 
 
 
- module.exports = app;
+module.exports = app;
