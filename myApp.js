3651fd42a7ee6160917a485d9419a3e4e65e60c4
Pakize Bozkurt <pakiozihak@gmail.com>
2023-01-12 23:15:49 +0000

    Hello World

--- myApp.js
+++ myApp.js
@@ -1,9 +1,21 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+
+app.use = (express.json());
+
+//const port = 3007;
+
+app.get("/", function(req, res) {
+    res.send("Hello World!")
+})
 
 
 
 
+//Server!
+// app.listen(port, () => {
+//   console.log(`Server is running port on ${port}`);
+// });
 
 
 
