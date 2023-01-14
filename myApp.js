75b8e60d97466aed5579b5e11808245913cadfe9
Hayat <hayatu4islam@yahoo.com>
2023-01-14 00:07:40 +0000

    Fix rendering error

--- myApp.js
+++ myApp.js
@@ -7,11 +7,11 @@ console.log("Hello World");
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/views/index.html");
-})
+});
 
 app.get("/json", function(req, res){
-    res.json({ message: "Hello json" });
-})
+    res.json({ "message": "Hello json" });
+});
 
 
 
