0dff98bfaaeabf23f01a9bbd46ccffc0846c5e05
Hayat <hayatu4islam@yahoo.com>
2023-01-14 00:34:18 +0000

    added .env file

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require("dotenv").config();
 
 app.use("/public", express.static(__dirname + "/public"));
 // Meet the Node console
@@ -10,7 +11,10 @@ app.get("/", function(req, res){
 });
 
 app.get("/json", function(req, res){
-    res.json({ "message": "Hello json" });
+    let msgObj = { message: "Hello json" };
+    if (process.env.MESSAGE_STYLE === "uppercase"){
+        res.json({ "message": "HELLO JSON"});
+    } else res.json({ message: "Hello json" });
 });
 
 
