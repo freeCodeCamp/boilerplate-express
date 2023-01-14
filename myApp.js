03d3df5a401c27fdc2145978336298ae5d9cda8f
Hayat <hayatu4islam@yahoo.com>
2023-01-14 01:10:08 +0000

    update

--- myApp.js
+++ myApp.js
@@ -12,9 +12,12 @@ app.get("/", function(req, res){
 
 app.get("/json", function(req, res){
     let msgObj = { message: "Hello json" };
-    if (process.env.MESSAGE_STYLE){
-        res.json({ "message": "HELLO JSON"});
-    } else res.json({ message: "Hello json" });
+    if (process.env.MESSAGE_STYLE === "uppercase"){
+        // res.json({"message": msgObj.message.toUpperCase()});
+        res.json({
+            message: "HELLO JSON"
+        });
+    } else res.json({"message": "Hello json"});
 });
 
 
