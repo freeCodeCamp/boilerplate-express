367173a018e1eefceef44b5a2ef6fad90691a073
Mohit <mohit.patel1966@gmail.com>
2023-02-05 23:00:16 +0530

    added handler started express

--- myApp.js
+++ myApp.js
@@ -1,41 +1,10 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log("Hello World");
+const handler = (req,res)=>{
+    res.send("Hello Express")
+} 
+app.get('/',handler)
 
  module.exports = app;
