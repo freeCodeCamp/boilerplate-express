673c645faa09cb10a3b972eb0ceb8a981729e1a8
Sanjay <116330923+emailsanjayrg@users.noreply.github.com>
2023-04-13 09:28:23 +0530

    Update myApp.js
    added static page - app.use(express.static(__dirname + '/public'))

--- myApp.js
+++ myApp.js
@@ -1,14 +1,26 @@
 let express = require('express');
 let app = express();
+
+
+
+
+
+
 console.log('Hello World')
+
 
 /*app.get("/", (request, response) => {
  response.send('Hello Express')
 })*/
 
+
 app.get("/", (request, response) => {
  response.sendFile(__dirname + '/views/index.html')
 })
+
+
+app.use(express.static(__dirname + '/public'))
+
 
 
 
