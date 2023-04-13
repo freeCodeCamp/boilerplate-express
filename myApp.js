bb23d7739f3a4e68030485f58bd8917761915a0d
Sanjay <116330923+emailsanjayrg@users.noreply.github.com>
2023-04-13 09:17:03 +0530

    Update myApp.js
    Changed to app.get("/", (request, response) => {
     response.sendFile(_dirname + '/view/index.html')
    })

--- myApp.js
+++ myApp.js
@@ -2,11 +2,13 @@ let express = require('express');
 let app = express();
 console.log('Hello World')
 
-app.get("/", (request, response) => {
+/*app.get("/", (request, response) => {
  response.send('Hello Express')
+})*/
+
+app.get("/", (request, response) => {
+ response.sendFile(_dirname + '/view/index.html')
 })
-
-
 
 
 
