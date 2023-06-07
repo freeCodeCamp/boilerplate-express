1408993b2da3e8b04086b6b9c82e8f5b92ea97b8
Replit user <>
2023-06-07 01:24:19 +0000

    activity 4 using .env route: /json will return uppercase message if env variable containt uppercase string

--- myApp.js
+++ myApp.js
@@ -9,7 +9,11 @@ app.get("/", function(req, res) {
 })
 
 app.get("/json",function(req,res){
-  res.json({"message": "Hello json"});
+  var message = "Hello json";
+  if(process.env.MESSAGE_STYLE == "uppercase"){
+    message = message.toUpperCase();
+  }
+  res.json({"message": message});
 })
 
 app.use("/public", express.static(__dirname + "/public"))
