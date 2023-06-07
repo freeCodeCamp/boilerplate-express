862b3245c3d8196f5d5237c161d5d455813e34ff
Replit user <>
2023-06-07 02:28:29 +0000

    Activity 9 Get Query Parameter Input from the Client using parameter in query

--- myApp.js
+++ myApp.js
@@ -33,16 +33,12 @@ app.get("/:word/echo",function(req,res){
   res.json({"echo" : req.params.word})
 })
 
-//app.get("/name",function(req,res){
-//  res.json({name : req.query.first + " " + req.query.last})
-//})
-
 app.route("/name")
   .get(function(req,res){
     res.json({name : req.query.first + " " + req.query.last});
   }).post(function(req,res){
     
-  })
+})
 
 app.use("/public", express.static(__dirname + "/public"))
 
