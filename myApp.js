9255f73d0019f58be97fb739f72dc53e0516dc6d
Replit user <>
2023-06-07 01:12:38 +0000

    exit exit() Activity 5

--- myApp.js
+++ myApp.js
@@ -8,6 +8,10 @@ app.get("/", function(req, res) {
   res.sendFile(absolutePath);
 })
 
+app.get("/json",function(req,res){
+  res.json({"message": "Hello json"});
+})
+
 app.use("/public", express.static(__dirname + "/public"))
 
 
