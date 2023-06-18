b011baaa43a053e7fd0eac80a65f535cfe399e39
@Ash9311 <ashutoshmulki7@gmail.com>
2023-06-18 23:23:30 +0530

    task 3

--- myApp.js
+++ myApp.js
@@ -7,7 +7,19 @@ app.get("/",function(req,res){
     res.send("Hello Express");
 })
 
+app.get("/",(req,res)=>{
+    res.send(__dirname+ "/public/index.html");
+});
 
+app.use(express.static(__dirname + "/public"));
+
+app.use("/public",express.static(__dirname + "/public"));
+
+app.get("/json",(req,res)=>{
+    res.json({
+        "message":"Hello json"
+    });
+});
 
 
 
