19a4406c04fa304cc0ed0dc772e799a34591a7a3
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-04 10:51:38 +0530

    4 tests are done

--- myApp.js
+++ myApp.js
@@ -1,39 +1,16 @@
 let express = require('express');
 let app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// app.get('/', (req, res) => {
+//     console.log("Hello, world!"); // Add this line
+//     res.send("Hello, world!");
+//   });
+const htmlFile = __dirname + '/views/index.html'
+function firstReq(req, res) {
+    res.sendFile(htmlFile);
+    // res.send('Hello Express')
+    
+}
+app.get('/', firstReq)
+ console.log("Hello World")
 
  module.exports = app;
