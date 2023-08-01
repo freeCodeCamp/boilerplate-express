ef4d4c3f577c70fe87fc73cf96a92fedfe27443b
Jocelle Bautista <jcllebautista@gmail.com>
2023-08-01 12:32:25 -0400

    Complete Challenge#4

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ app.get("/", (require, res) => {
   res.sendFile(__dirname + "/views/index.html")
 })
 
-
+app.use("/public", express.static(__dirname + "/public"))
 
  module.exports = app;
 //  http://localhost:3000/
