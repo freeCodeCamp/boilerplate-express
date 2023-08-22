b1a1da1089467a57a6278cb9ceb9cf6f62fd7936
Hafiz Mohammad Danmanu <53683648+Danmanu44@users.noreply.github.com>
2023-08-22 23:00:31 +0100

    init

--- myApp.js
+++ myApp.js
@@ -1,5 +1,41 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
+const port = 3000
+const html_file_path= __dirname+'/views/index.html';
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    console.log(`Hello World`)
+
+  //res.send("Hello Express")
+  res.sendFile(html_file_path)
+console.log(`Example app listening on port ${port}`)
+
+})
+
+app.get('/json',(req,res)=>{
+  if(process.env.MESSAGE_STYLE=='uppercase'){
+    res.json({
+      "message":"HELLO JSON"
+  });
+
+  }
+  else{
+    res.json({
+      "message":"Hello Json"
+  });
+  }
+
+    
+
+
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 
 
