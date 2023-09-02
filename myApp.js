cc56cee8092ec5cf8babcff8a0d1c700e30749af
Peter-TMK <tundepeter01@gmail.com>
2023-09-02 13:27:33 +0100

    first commit

--- myApp.js
+++ myApp.js
@@ -1,39 +1,6 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
