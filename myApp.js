46ef8f9eebc7ccd845b4056f9570d9b90fcbbd4e
Kanon11 <kanoncuetcse11@gmail.com>
2023-11-12 23:41:54 +0600

    fileresponse

--- myApp.js
+++ myApp.js
@@ -1,8 +1,9 @@
 let express = require('express');
 let app = express();
 console.log("Hello World")
+console.log(__dirname+'views/index.html')
 app.get("/", (req, res) => {
-    res.send("Hello Express")
+    res.sendFile(__dirname +'/views/index.html')
 })
 
 
