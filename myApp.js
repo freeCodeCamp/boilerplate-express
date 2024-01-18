c7d5238cc41764f4ba4a62a64f7b075cf7484310
Kaif Kazi <63505140+Kaif09@users.noreply.github.com>
2024-01-18 14:48:09 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,12 +1,26 @@
 let express = require("express");
 let app = express();
+require("dotenv").config();
 
 absolutePath = __dirname + "/views/index.html";
+
+app.use(function middleware(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
 
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(absolutePath);
+});
+
+app.get("/json", function (req, res) {
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
 });
 
 console.log("Hello World");
