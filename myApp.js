c5dc072713458d781de9b22b2c9057a974c4e612
Glitch (elite-ajar-grape) <none>
2024-01-30 11:04:13 +0000

    ⬜🎗️ Updated with Glitch

--- myApp.js
+++ myApp.js
@@ -1,39 +1,5 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
