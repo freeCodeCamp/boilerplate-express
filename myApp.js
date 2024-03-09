153dd398cf4949e7812703ce32929c21761ed2da
Saifeddine Elhanoune <saifeddine13elhanoune@gmail.com>
2024-03-09 00:42:28 +0000

    in changes

--- myApp.js
+++ myApp.js
@@ -1,39 +1,8 @@
 let express = require('express');
 let app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/', (req, res) => {
+  res.send("Hello World");
+})
 
  module.exports = app;
