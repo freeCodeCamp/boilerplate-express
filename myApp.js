be5478424f7bae20ae9dcb064012e8793c7705ed
Saifeddine Elhanoune <saifeddine13elhanoune@gmail.com>
2024-03-09 03:31:54 +0000

    serve an HTML file

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ let express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-  res.send("Hello World");
+  absolutePath = __dirname + '/views/index.html';
+  res.sendFile(absolutePath);
 })
 
  module.exports = app;
