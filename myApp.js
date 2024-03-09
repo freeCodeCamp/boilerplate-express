9630cca52d48ffed17295864a7282e4d46998381
Saifeddine Elhanoune <saifeddine13elhanoune@gmail.com>
2024-03-09 03:39:55 +0000

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -3,7 +3,8 @@ let app = express();
 
 app.get('/', (req, res) => {
   absolutePath = __dirname + '/views/index.html';
-  res.sendFile(absolutePath);
+  // res.sendFile(absolutePath);
+  app.use('/public', express.static(__dirname + '/public'));
 })
 
  module.exports = app;
