4f6da632f2d7d6de03ec4da75ef6e6bbbd572255
Olga Tsybikova <tsy2701@gmail.com>
2024-03-15 11:15:16 +0100

    Added handlers to show html and css

--- myApp.js
+++ myApp.js
@@ -1,39 +1,16 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
+/* app.get("/", (req, res) => {
+  res.send("Hello Express");
+}); */
+//show html on local host
+absolutePath = __dirname + "/views/index.html";
+app.get("/", (req, res) => {
+  res.sendFile(absolutePath);
+});
+cssPath = __dirname + "/public";
+app.use("/public", express.static(cssPath));
