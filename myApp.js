c7957217ea755c1cc6f278fd60e49cc94ba0c02b
Cecilia <ccchimento@gmail.com>
2024-06-07 22:03:31 -0600

    25% complete

--- myApp.js
+++ myApp.js
@@ -1,9 +1,15 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World")
 
+// app.get('/', function(req, res){
+//     res.send('Hello Express')
+// })
 
-
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/views/index.html')
+})
 
 
 
