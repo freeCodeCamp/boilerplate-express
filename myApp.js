66de86b872e148f1d58a5e7b4bfdcac6b4b0f252
Cecilia <ccchimento@gmail.com>
2024-06-07 22:14:12 -0600

    42% complete

--- myApp.js
+++ myApp.js
@@ -11,8 +11,9 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/views/index.html')
 })
 
+app.use('/public', express.static(__dirname + '/public'))
 
-
+app.get('/json', function(req, res){res.json({"message": "Hello json"})})
 
 
 
