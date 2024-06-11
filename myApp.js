e5f794c40f6ec33fd151913ea32df70d9ed3d9c7
Cecilia <ccchimento@gmail.com>
2024-06-10 21:21:19 -0600

    50%

--- myApp.js
+++ myApp.js
@@ -1,19 +1,20 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
-console.log("Hello World")
-
-// app.get('/', function(req, res){
-//     res.send('Hello Express')
-// })
-
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html')
 })
 
 app.use('/public', express.static(__dirname + '/public'))
 
-app.get('/json', function(req, res){res.json({"message": "Hello json"})})
+app.get('/json', function (req, res) {
+    if(process.env.MESSAGE_STYLE === "uppercase"){
+        res.json({ "message": "HELLO JSON" })
+    } else {
+        res.json({ "message": "Hello json" }) 
+    }
+})
 
 
 
@@ -43,4 +44,9 @@ app.get('/json', function(req, res){res.json({"message": "Hello json"})})
 
 
 
- module.exports = app;
+
+
+
+
+
+module.exports = app;
